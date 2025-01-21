d341310050520f2c15c3ef60c886101b5e6aa0eb
Ignitfire <josuesoubeyran2000@gmail.com>
2025-01-21 10:09:05 +0100

    finition du template général et de la page contact

---
+++ frontend/src/app/core/feature/contact/contact.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {TextBoxComponent} from '../../ui/text-box/text-box.component';
+import {CardComponent} from '../../ui/card/card.component';
+
+@Component({
+  selector: 'app-contact',
+  imports: [
+    TextBoxComponent,
+    CardComponent
+  ],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
---
+++ frontend/src/app/core/feature/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {TextBoxComponent} from '../../ui/text-box/text-box.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    TextBoxComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
